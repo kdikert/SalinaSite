5f981299cbb0cd1cf31007771d08c9cd1495a131
kdikert <kdikert@cc.hut.fi>
2012-06-12 02:41:59 +0300

    Admin improvements.

--- src/salinaadmin/views.py
+++ src/salinaadmin/views.py
@@ -1,8 +1,11 @@
 
+from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+import django.contrib.auth.views
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-import django.contrib.auth.views
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 
 @login_required
@@ -11,6 +14,9 @@ def index(request):
 
 
 def login(request):
+    if request.GET.get(auth.REDIRECT_FIELD_NAME) == reverse(logout):
+        return HttpResponseRedirect(reverse(login))
+    
     return django.contrib.auth.views.login(request, template_name='admin/login.html')
 
 
