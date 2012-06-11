fb445d03abd638e1e052f277a5a28712ddb9a2fe
kdikert <kdikert@cc.hut.fi>
2012-06-12 01:45:53 +0300

    Changed to use template views.

--- src/salina/urls.py
+++ src/salina/urls.py
@@ -1,10 +1,17 @@
 
 from django.conf.urls import patterns, url
 
+from django.views.generic import TemplateView
+
+
 urlpatterns = patterns('salina.views',
-     url(r'^$', 'index', name='index'),
-     url(r'^about$', 'about', name='about'),
-     url(r'^atelier$', 'atelier', name='atelier'),
-     url(r'^webshop$', 'webshop', name='webshop'),
-     url(r'^contact$', 'contact', name='contact'),
+
+     url(r'^$', TemplateView.as_view(template_name="salina/index.html"), name='index'),
+     url(r'^about$', TemplateView.as_view(template_name="salina/about.html"), name='about'),
+     
+     url(r'^atelier$', TemplateView.as_view(template_name="salina/atelier.html"), name='atelier'),
+     url(r'^atelier/(?P<category_slug>[0-9a-zA-Z\d_-]+)/?$', 'atelier_category', name='atelier_category'),
+     
+     url(r'^webshop$', TemplateView.as_view(template_name="salina/webshop.html"), name='webshop'),
+     url(r'^contact$', TemplateView.as_view(template_name="salina/contact.html"), name='contact'),
 )
