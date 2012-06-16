bc72794085d2a985e7eb2afea16d6a3067a42e26
kdikert <kdikert@cc.hut.fi>
2012-06-16 14:44:02 +0300

    Added admin for products

--- src/salina/admin.py
+++ src/salina/admin.py
@@ -1,9 +1,15 @@
 
 from django.contrib import admin
 
+from .models import Material
+from .models import ProductGroup
+from .models import Product
 from .models import CMSText
 from .models import CMSTranslation
 
+admin.site.register(Material)
+admin.site.register(ProductGroup)
+admin.site.register(Product)
 admin.site.register(CMSText)
 admin.site.register(CMSTranslation)
 
