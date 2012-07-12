3b3bd492a015b2bbf9d41de1c8ec5fc1a5c03afe
kdikert <kdikert@cc.hut.fi>
2012-07-12 19:49:45 +0300

    New site layout

--- src/salina/static/salina/js/cms.js
+++
@@ -1,5 +0,0 @@
-
-$(document).ready(function() {
-	$(".cmscontainer h1 + p").addClass("cmscontainer_first");
-	//$(".cmscontainer p:last-child").addClass("cmscontainer_last");
-});
--- src/salina/views.py
+++ src/salina/views.py
@@ -16,9 +16,11 @@ def atelier(request):
 def atelier_category(request, group_id):
     product_group = get_object_or_404(ProductGroup, group_id=group_id)
     products = Product.objects.filter_displayed().filter(product_group=product_group)
+    product_groups = ProductGroup.objects.all()
     
     return render_to_response("salina/atelier_category.html",
-                              {'product_group' : product_group,
+                              {'product_groups' : product_groups,
+                               'product_group' : product_group,
                                'products' : products},
                               context_instance=RequestContext(request))
 
